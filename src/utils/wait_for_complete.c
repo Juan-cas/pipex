a992e49cf316b02733b7d958164fffea8c2f6749
juan-cas <juan-cas@student.42madrid.com>
2024-05-28 05:45:53 +0200

    comented the errors from the child processes and also discovered a bug where if i do make bonus and then make it doesnt rebuild the executable

--- src/utils/wait_for_complete.c
+++ src/utils/wait_for_complete.c
@@ -6,7 +6,7 @@
 /*   By: juan-cas <juan-cas@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 15:24:06 by juan-cas          #+#    #+#             */
-/*   Updated: 2024/05/22 09:31:01 by juan-cas         ###   ########.fr       */
+/*   Updated: 2024/05/28 04:53:45 by juan-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,22 @@ void wait_for_children(t_env *information)
 	while (i--)
 	{
 		check_status = waitpid(-1, &status, 0);
-		if (check_status == -1) {
-			if (errno == ECHILD) {
-				printf("No child processes to wait for.\n");
-			} else {
-				perror("waitpid");
-			}
-		} else {
-			printf("Child process with PID %d changed state.\n", check_status);
-			if (WIFEXITED(status)) {
-				printf("Child exited with status %d.\n", WEXITSTATUS(status));
-			} else if (WIFSIGNALED(status)) {
-				printf("Child killed by signal %d.\n", WTERMSIG(status));
-			} else if (WIFSTOPPED(status)) {
-				printf("Child stopped by signal %d.\n", WSTOPSIG(status));
-			}
-		}
+//		if (check_status == -1) {
+//			if (errno == ECHILD) {
+//				printf("No child processes to wait for.\n");
+//			} else {
+//				perror("waitpid");
+//			}
+//		} else {
+//			printf("Child process with PID %d changed state.\n", check_status);
+//			if (WIFEXITED(status)) {
+//				printf("Child exited with status %d.\n", WEXITSTATUS(status));
+//			} else if (WIFSIGNALED(status)) {
+//				printf("Child killed by signal %d.\n", WTERMSIG(status));
+//			} else if (WIFSTOPPED(status)) {
+//				printf("Child stopped by signal %d.\n", WSTOPSIG(status));
+//			}
+//		}
 		if (check_status == 0)
 			break ;
 	}
